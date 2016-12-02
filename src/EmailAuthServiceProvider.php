064c778aa69b3c2746b2047e7cc40d8ce3031a40
Brian Faust <hello@brianfaust.de>
2016-12-02 20:44:12 +0200

    Rename Service Provider

--- src/EmailAuthServiceProvider.php
+++ src/EmailAuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace BrianFaust\EmailAuth;
 
-class ServiceProvider extends \BrianFaust\ServiceProvider\ServiceProvider
+use BrianFaust\ServiceProvider\ServiceProvider;
+
+class EmailAuthServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
