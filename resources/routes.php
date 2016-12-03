874dfbab421cb826d5b1d987580dd4057c0b396c
Brian Faust <hello@brianfaust.de>
2016-12-03 06:38:58 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- resources/routes.php
+++ resources/routes.php
@@ -1,6 +1,15 @@
 <?php
 
+/*
+ * This file is part of Laravel E-Mail Authentication.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 Route::get(config('email-authenticate.route.uri'), [
-    'as' => config('email-authenticate.route.as'),
+    'as'   => config('email-authenticate.route.as'),
     'uses' => config('email-authenticate.route.uses'),
 ]);
