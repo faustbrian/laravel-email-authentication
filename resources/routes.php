40836f927389694e5d5e3fd1bcb6d3758ca5126d
Brian Faust <hello@brianfaust.de>
2017-04-22 08:45:35 +0300

    Try to fix the mess StyleCI makes

--- resources/routes.php
+++ resources/routes.php
@@ -1,13 +1,6 @@
 <?php
 
-/*
- * This file is part of Laravel E-Mail Authentication.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 Route::get(config('email-authenticate.route.uri'), [
     'as'   => config('email-authenticate.route.as'),
